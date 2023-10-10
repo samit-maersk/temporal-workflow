4cd97e27f070b4ae812a1bfbe33930532ba54a23
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2023-10-10 17:01:15 +0200

    Test fix

--- src/test/java/com/example/temporalworkflow/TemporalWorkflowApplicationTests.java
+++ src/test/java/com/example/temporalworkflow/TemporalWorkflowApplicationTests.java
@@ -2,10 +2,23 @@ package com.example.temporalworkflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
 
 @SpringBootTest
+@Testcontainers
 class TemporalWorkflowApplicationTests {
 
+	@Container
+	static DockerComposeContainer<?> TEMPORAL =
+			new DockerComposeContainer(new File("docker-compose.yml"))
+					//.waitingFor("temporal", Wait.forListeningPort())
+					.withLocalCompose(true)
+					.waitingFor("temporal", Wait.forLogMessage(".*\"Namespace\":\"default\".*", 1));
 	@Test
 	void contextLoads() {
 	}
